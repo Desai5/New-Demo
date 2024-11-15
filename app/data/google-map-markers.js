3552d33fa5a926e09de4e6bd7aed0ea90804709e
Eesh Desai <eeshdesai@gmail.com>
2024-11-14 21:08:36 -0800

    I HATE JAVASCRIPTgit status -sgit status -s AHHHHHHHHHHHHHHHHHHHHHHHgit status -sgit status -sgit status -sgit status -s

---
+++ app/data/google-map-markers.js
@@ -0,0 +1,62 @@
+
+   [
+    {
+      "name": "The Last City",
+      "location": "Earth",
+      "size": "Large",
+      "Difficulty":"Easy",
+      "Requires":"No DLC"
+    },
+    {
+      "name": "EDZ",
+      "location": "Outer Space",
+      "size": "Large",
+      "Difficulty":"Medium",
+      "Requires":"No DLC"
+    },
+    {
+      "name": "Legend",
+      "location": "Depending on Event location changes",
+      "size": "Large",
+      "Difficulty":"Legendary",
+      "Requires":"DLC"
+    },
+
+    {
+      "name": "The Tower",
+      "location": "Earth",
+      "size": "Large",
+      "Difficulty":"Medium",
+      "Requires":"No DLC"
+    },
+    {
+      "name": "The Crucible",
+      "location": "Mars",
+      "size": "Medium",
+      "Difficulty": "Legendary",
+      "Requires":"No DLC"
+    },
+    {
+      "name": "Europa",
+      "location": "Europa",
+      "size": "Large",
+      "Difficulty": "Medium",
+      "Requires":"DLC"
+      
+    },
+    {
+      "name": "Nessus",
+      "location": "Nessus",
+      "size": "Large",
+      "Difficulty":"Legendary",
+      "Requires":"No DLC"
+    },
+    {
+      "name": "The Final Shape",
+      "location": "The Pale Heart",
+      "size": "Large",
+      "Difficulty":"Legendary",
+      "Requires":"No DLC"
+    }
+  ]
+
